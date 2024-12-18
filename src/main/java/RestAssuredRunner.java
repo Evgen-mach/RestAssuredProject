6bcc9c6e5ea4d0bbef6fc3ecc32bff93ffdeb9a8
Yauheni Machys <y.machys@andersenlab.com>
2024-12-18 18:32:08 +0300

    RA HW First

---
+++ src/main/java/RestAssuredRunner.java
@@ -0,0 +1,11 @@
+import io.restassured.RestAssured;
+
+public class RestAssuredRunner {
+    public static void main(String[] args) {
+        RestAssured.given()
+                .log().all()
+                .baseUri("https://api.trello.com")
+                .get()
+                .prettyPeek();
+    }
+}
