2c49e99aa9efda807b25af3fb9a19ca9d05c13a5
Yauheni Machys <y.machys@andersenlab.com>
2024-12-26 23:28:22 +0300

    RA HW 5th

--- src/test/java/GetBoardsTest.java
+++ src/test/java/GetBoardsTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
@@ -24,19 +25,23 @@ public class GetBoardsTest {
     @Test
     public void checkGetBoards() {
         requestWithAuth()
+                .queryParam("fields", "id,name")
                 .pathParam("memberId", "yauhenim3")
                 .get("/1/members/{memberId}/boards")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/get_boards.json"));
     }
 
     @Test
     public void checkGetBoard() {
         requestWithAuth()
+                .queryParam("fields", "id,name")
                 .pathParam("boardId", "675067bee6c044df8c6dadd3")
                 .get("/1/boards/{boardId}")
                 .then()
                 .statusCode(200)
-                .body("name", Matchers.equalTo("Test"));
+                .body("name", Matchers.equalTo("Test"))
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/get_board.json"));
     }
 }
