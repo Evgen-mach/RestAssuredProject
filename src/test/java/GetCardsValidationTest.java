bb531c8b727bc5cc5b15e82ae0e3643ab0e68615
Yauheni Machys <y.machys@andersenlab.com>
2025-01-03 14:19:25 +0300

    Upd objectId to id

--- src/test/java/GetCardsValidationTest.java
+++ src/test/java/GetCardsValidationTest.java
@@ -9,8 +9,8 @@ public class GetCardsValidationTest extends BaseTest{
     @Test
     public void checkGetCardWithInvalidId() {
         Response response = requestWithAuth()
-                .pathParam("cardId", "invalid")
-                .get("/1/cards/{cardId}");
+                .pathParam("id", "invalid")
+                .get("/1/cards/{id}");
         response
                 .then()
                 .statusCode(400);
@@ -20,8 +20,8 @@ public class GetCardsValidationTest extends BaseTest{
     @Test
     public void checkGetCardWithInvalidAuth() {
         Response response = requestWithoutAuth()
-                .pathParam("cardId", "67582a66bd72066b2a732bed")
-                .get("/1/cards/{cardId}");
+                .pathParam("id", "67582a66bd72066b2a732bed")
+                .get("/1/cards/{id}");
         response
                 .then()
                 .statusCode(401);
@@ -35,8 +35,8 @@ public class GetCardsValidationTest extends BaseTest{
                         "key", "6dfe2176e5f391f8414d73603b6a9f77",
                         "token", "ATTA6cd39dd9b6c03c1bdca8c6c0db90a355f3d5b25ae8fcb8a43833a512af78ea647CF220BE"
                 ))
-                .pathParam("cardId", "67582a66bd72066b2a732bed")
-                .get("/1/cards/{cardId}");
+                .pathParam("id", "67582a66bd72066b2a732bed")
+                .get("/1/cards/{id}");
         response
                 .then()
                 .statusCode(401);
