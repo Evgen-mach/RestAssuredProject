1bcfef2d2baf98733ae69d7bbb69428b9e204cf4
Yauheni Machys <y.machys@andersenlab.com>
2025-01-09 22:17:08 +0300

    RA HW 10th

--- src/test/java/consts/UrlParamValues.java
+++ src/test/java/consts/UrlParamValues.java
@@ -14,9 +14,12 @@ public class UrlParamValues {
 
     public static final Map<String, String> ANOTHER_USER_AUTH_QUERY_PARAMS = Map.of(
             "key", "6dfe2176e5f391f8414d73603b6a9f77",
-            "token", "ATTA6cd39dd9b6c03c1bdca8c6c0db90a355f3d5b25ae8fcb8a43833a512af78ea647CF220BE"
+            "token", "ATTA6cd39dd9b6c03c1bdca8c6c0db90a355f3d5b25ae8fcb8a43833a512af78ea647CF220BZ"
     );
 
     public static final String EXISTING_BOARD_ID = "675067bee6c044df8c6dadd3";
     public static final String USER_NAME = "yauhenim3";
+
+    public static final String EXISTING_CARD_ID = "67582a66bd72066b2a732bed";
+    public static final String EXISTING_LIST_ID = "6758283447a428b4f2bc66b8";
 }
