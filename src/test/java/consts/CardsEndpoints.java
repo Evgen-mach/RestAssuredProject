7d794f9f3ff615bb07c3d737174684f2c10e2e07
Yauheni Machys <y.machys@andersenlab.com>
2025-01-14 16:08:38 +0300

    RA HW 11th

--- src/test/java/consts/CardsEndpoints.java
+++ src/test/java/consts/CardsEndpoints.java
@@ -4,4 +4,8 @@ public class CardsEndpoints {
 
     public static final String GET_ALL_CARDS_URL = "/1/lists/{id}/cards";
     public static final String GET_CARD_URL = "/1/cards/{id}";
+
+    public static final String CREATE_CARD_URL = "/1/cards";
+
+    public static final String DELETE_CARD_URL = "/1/card/{id}";
 }
